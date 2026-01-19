2f69702cd4e69b66a8ba71dc0f4630c1e21d9449
Nataliia Antonova <5555482@gmail.com>
2026-01-19 13:59:01 +0000

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 
+const initialQuery = "What are the presidents of the United States?"
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [userMessage, setUserMessage] = useState < string > (initialQuery)
+  const [userHistory, setUserHistory] = useState < string[] > ([])
+  const [isThinking, setIsThinking] = useState < boolean > (false)
+
+  const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setUserHistory([...userHistory, userMessage]);
+    setIsThinkinga(true);
+    setUserMessage('');
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <h1>LLM chat</h1>
+        <ul>{userHistory.map(message =>
+          <p>
+            <li>
+              {message}
+            </li>
+          </p>)}
+
+        </ul>
+        <form onSubmit={onFormSubmit}>
+          <input
+            type="text" placeholder="Type your message..."
+            value={userMessage} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUserMessage(e.target.value)}
+            name="userQuery"
+          />
+          <button type="submit" disabled={isThinking}>
+            Send
+          </button>
+        </form>
+      </nav>
     </>
   )
 }
